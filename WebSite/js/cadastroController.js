a2649e37888b2005f709618013191d18eccd6cd6
sfspacov <sfspacov@gmail.com>
2016-06-10 11:54:33 -0300

    Alteração em controller angular
    Cancelar edição ao clicar em editar

--- WebSite/js/cadastroController.js
+++ WebSite/js/cadastroController.js
@@ -14,6 +14,7 @@ angular.module('cadastro', ["ui.bootstrap"])
           Model: {},
           Cancelar: function () {
               $scope.Veiculo.Model = {};
+              $scope.Editar = false;
           },
           Deletar: function (id) {
 
